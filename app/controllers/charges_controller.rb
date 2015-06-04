03559e147860d6889829ce034b2badfc8f7a3a76
snugthejoiner <joiner@jedparish.com>
2015-06-03 20:00:20 -0400

    more stripe config

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,7 +3,7 @@ class ChargesController < ApplicationController
   def new
     @stripe_btn_data = {
      key: "#{ Rails.configuration.stripe[:publishable_key] }",
-     description: "BigMoney Membership - #{current_user.username}",
+     description: "Premium Membership - #{current_user.email}",
      amount: 100
    }
   end
